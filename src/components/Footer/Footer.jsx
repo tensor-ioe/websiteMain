5ac8b4463277018b8d0ddf665a0425c7116bb0a2
Kashmir Shrestha <cthakashmir999@gmail.com>
2025-01-22 09:01:50 +0545

    Tensor v1.3 --header, footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,30 +13,26 @@ const Footer = () => {
         ["https://www.facebook.com/TensorIOE", facebook, 'TensorIOE'],
         ["https://www.linkedin.com/company/tensorioe/?fbclid=PAZXh0bgNhZW0CMTEAAaYRkBakT16ytQ9q6E_0lpo49NCCaEz5WYdnztbNrWV9P37ytK0dTxGNh-c_aem_gy2aLNm2PShqwl1tnZU3kg", linkedin, 'TensorIoe']
     ]
-
-    const scrollToTop = () => {
-        window.scrollTo(0, 0)
-    }
-
+    
     return (
         <div className="w-full relative z-20">
             <svg viewBox="0 150 1440 120">
                 <path fill="#10518A" fillOpacity="1" d="M0,224L60,234.7C120,245,240,267,360,250.7C480,235,600,181,720,170.7C840,160,960,192,1080,202.7C1200,213,1320,203,1380,197.3L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
             </svg>
             <div className="w-full mt-[-5px] bg-footerbgcolor">
-                <div className="w-width h-[300px] flex justify-between items-center mx-auto">
-                    <div className="flex flex-col items-center gap-[22.5px] mr-16">
-                        <h1 className="text-[30px] font-extrabold">Follow us</h1>
+                <div className="w-full px-4 h-[300px] flex justify-between items-center mx-auto xl:w-width xl:p-0">
+                    <div className="flex flex-col items-center gap-[22.5px] xl:mr-16">
+                        <h1 className="text-3xl xl:text-[30px] font-extrabold">Follow us</h1>
                         {socialMedia.map((item, index) => {
                             return (
-                                <a key={index} className="w-full flex items-center justify-between text-[18px]" target='_blank' rel="noopener noreferrer" href={item[0]}>
-                                    <img src={item[1]} alt="" height={`25px`} width={'25px'} />
+                                <a key={index} className="w-full flex items-center justify-between text-base xl:text-[18px]" target='_blank' rel="noopener noreferrer" href={item[0]}>
+                                    <img className="w-[20px] h-[20px] xl:w-[25px] xl:h-[25px]" src={item[1]} alt="" />
                                     {item[2]}
                                 </a>
                             )
                         })}
                     </div>
-                    <div className="flex flex-col items-center">
+                    <div className="hidden xl:flex flex-col items-center">
                         <h1 className="text-[30px] font-extrabold">Sitemap</h1>
                         {pages.map((page, index) => {
                             return (
@@ -45,15 +41,15 @@ const Footer = () => {
                         })}
                     </div>
                     <div className="flex flex-col items-center gap-[15px]">
-                        <NavLink to='/' onClick={scrollToTop}>
-                            <img src={logo} alt="Tensor" height='90px' width='90px' />
+                        <NavLink to='/' onClick={() => { window.scrollTo(0, 0)}}>
+                            <img className="w-[60px] h-[60px]  xl:w-[90px] xl:h-[90px]" src={logo} alt="Tensor" />
                         </NavLink>
-                        <a className="flex gap-[10px] text-[18px]" target='_blank' rel="noopener noreferrer" href="https://www.google.com/maps/place/IOE,+Thapathali+Campus/@27.6941071,85.3186142,19z/data=!4m6!3m5!1s0x39eb19ae08c068d9:0x475bed1f66d060c!8m2!3d27.6940347!4d85.3188261!16s%2Fm%2F05zhjq0?entry=ttu&g_ep=EgoyMDI0MDkyNC4wIKXMDSoASAFQAw%3D%3D">
-                            <img src={gps} alt="Location" width='20px' />
+                        <a className="flex gap-[10px] text-base xl:text-[18px]" target='_blank' rel="noopener noreferrer" href="https://www.google.com/maps/place/IOE,+Thapathali+Campus/@27.6941071,85.3186142,19z/data=!4m6!3m5!1s0x39eb19ae08c068d9:0x475bed1f66d060c!8m2!3d27.6940347!4d85.3188261!16s%2Fm%2F05zhjq0?entry=ttu&g_ep=EgoyMDI0MDkyNC4wIKXMDSoASAFQAw%3D%3D">
+                            <img className="w-4 xl:w-[20px]" src={gps} alt="Location" width='20px' />
                             IOE THAPATHALI
                         </a>
-                        <a className="flex gap-[10px] text-[18px]" target='_blank' rel="noopener noreferrer" href="mailto:tensor@tcioe.edu">
-                            <img src={mail} alt="Mail" width='35px' />
+                        <a className="flex gap-[10px] text-base xl:text-[18px]" target='_blank' rel="noopener noreferrer" href="mailto:tensor@tcioe.edu">
+                            <img className="w-7 xl:w-[35px]" src={mail} alt="Mail" width='35px' />
                             tensorioe@tcioe.edu
                         </a>
                     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,46 @@
 import { NavLink } from "react-router-dom"
+import { useState } from "react"
 import logo from "../../images/assets/logo.png"
 import pages from '../../constants/pages'
 
 function Navbar() {
+    const [toggle, setToggle] = useState(false)
+    const handleToggle = () => setToggle((prev) => !prev)
+    
     return (
-        <div className="w-full h-[75px] grid place-content-center shadow-navbarShadow sticky top-0 z-20 bg-white">
-            <div className="w-width h-[55px] flex items-center justify-between">
-                <NavLink className="grid place-content-center" to='/'>
-                    <img src={logo} alt="Tensor" height='50px' width='165px'/>
+        <div className={`h-[75px]  grid place-content-center ${toggle ? 'mb-[250px]' : 'mb-[0px]'} shadow-navbarShadow sticky top-0 z-30 duration-300 bg-white xl:mb-0`}>
+            <div className="w-[100vw] h-[75px] px-4 flex items-center justify-between relative z-20 bg-white xl:w-width xl:p-0">
+                <NavLink className="grid place-content-center" to='/' onClick={() => { window.scrollTo(0, 0) }}>
+                    <img src={logo} alt="Tensor" height='50px' width='165px' />
                 </NavLink>
-                <div>
-                    {pages.map((page,index) => {
+                <div className='hidden xl:block'>
+                    {pages.map((page, index) => {
                         return (
                             <NavLink key={index} className={
                                 `text-[18px] no-underline px-[14px] py-[14px] hover:opacity-100
-                                ${(index === (pages.length -1)) ? `opacity-100 bg-applybgcolor rounded-lg` : 'opacity-55'}
+                                ${(index === (pages.length - 1)) ? `opacity-100 bg-applybgcolor rounded-lg` : 'opacity-55'}
                                 `
                             } to={`/${page.toLowerCase()}`}>{page}</NavLink>
                         )
                     })}
                 </div>
+                <div onClick={handleToggle} className="flex flex-col gap-[7px] items-center justify-center cursor-pointer xl:hidden">
+                    <div className="w-10 h-1 bg-[#1360A5] rounded-sm"></div>
+                    <div className="w-10 h-1 bg-[#1360A5] rounded-sm"></div>
+                    <div className="w-10 h-1 bg-[#1360A5] rounded-sm"></div>
+                </div>
+            </div>
+            {/* for responsive navbar */}
+            <div className={`w-[100vw] bg-white absolute z-10 ${toggle ? 'top-[101%]' : 'top-[-350%]'} duration-300 xl:hidden`}>
+                {pages.map((page, index) => {
+                    return (
+                        <NavLink key={index} className={
+                            `text-[14px] no-underline px-[14px] py-[14px] flex flex-row justify-center hover:opacity-100
+                                ${(index === (pages.length - 1)) ? `opacity-100 bg-applybgcolor` : 'opacity-55'}
+                                `
+                        } to={`/${page.toLowerCase()}`}>{page}</NavLink>
+                    )
+                })}
             </div>
         </div>
     )
