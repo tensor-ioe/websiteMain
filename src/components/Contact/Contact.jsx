3f2621ac38fee2a5ab6d89b6a050202144e9f210
sameekshadhakal <d.samiksha13@gmail.com>
2025-01-24 20:14:12 +0545

    Added the components for contact us page.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import ContactUsForm from './contactUsForm';
+import ContactUsMap from './contactUsMap';
+import './contact.css';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact-container">
+      <h1 className="contact-title">CONTACT US</h1>
+      <div className="contact-content">
+        <ContactUsForm />
+        <ContactUsMap />
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
