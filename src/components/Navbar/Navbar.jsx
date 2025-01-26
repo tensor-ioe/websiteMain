47c9f8b899351a61d2aa8e4fc582ed05b81f0435
Kashmir Shrestha <cthakashmir999@gmail.com>
2025-01-26 14:02:58 +0545

    Tensor v1.4 -- responsive design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,16 @@ import pages from '../../constants/pages'
 
 function Navbar() {
     const [toggle, setToggle] = useState(false)
-    const handleToggle = () => setToggle((prev) => !prev)
-    
+    const [start, setStart] = useState(true)
+    const handleToggle = () => {
+        setStart(false) /* to remove startup animation from hamburger to cross*/
+        setToggle((prev) => !prev)
+    }
     return (
-        <div className={`h-[75px]  grid place-content-center ${toggle ? 'mb-[250px]' : 'mb-[0px]'} shadow-navbarShadow sticky top-0 z-30 duration-300 bg-white xl:mb-0`}>
-            <div className="w-[100vw] h-[75px] px-4 flex items-center justify-between relative z-20 bg-white xl:w-width xl:p-0">
+        <div className={`h-[75px]  grid place-content-center ${toggle ? 'mb-[250px]' : 'mb-[0px]'} shadow-navbarShadow sticky top-0 z-30 duration-500 bg-white xl:mb-0`}>
+            <div className="w-[100vw] h-[75px] px-5 flex items-center justify-between relative z-20 bg-white xl:w-width xl:p-0">
                 <NavLink className="grid place-content-center" to='/' onClick={() => { window.scrollTo(0, 0) }}>
-                    <img src={logo} alt="Tensor" height='50px' width='165px' />
+                    <img className="max-sm:w-[130px] max-sm:h-[38px] w-[165px] h-[45px]" src={logo} alt="Tensor" />
                 </NavLink>
                 <div className='hidden xl:block'>
                     {pages.map((page, index) => {
@@ -25,13 +28,13 @@ function Navbar() {
                     })}
                 </div>
                 <div onClick={handleToggle} className="flex flex-col gap-[7px] items-center justify-center cursor-pointer xl:hidden">
-                    <div className="w-10 h-1 bg-[#1360A5] rounded-sm"></div>
-                    <div className="w-10 h-1 bg-[#1360A5] rounded-sm"></div>
-                    <div className="w-10 h-1 bg-[#1360A5] rounded-sm"></div>
+                    <div className={`w-10 h-1 bg-[#1360A5] rounded-sm ${start ? 'animate-none' : (toggle ? 'animate-topAnim' : 'animate-topAnimRev')}`}></div>
+                    <div className={`w-10 h-1 bg-[#1360A5] rounded-sm ${start ? 'animate-none' : (toggle ? 'animate-midAnim' : 'animate-midAnimRev')}`}></div>
+                    <div className={`w-10 h-1 bg-[#1360A5] rounded-sm ${start ? 'animate-none' : (toggle ? 'animate-botAnim' : 'animate-botAnimRev')}`}></div>
                 </div>
             </div>
             {/* for responsive navbar */}
-            <div className={`w-[100vw] bg-white absolute z-10 ${toggle ? 'top-[101%]' : 'top-[-350%]'} duration-300 xl:hidden`}>
+            <div className={`w-[100vw] bg-white absolute z-10 ${toggle ? 'top-[101%]' : 'top-[-350%]'} duration-500 xl:hidden`}>
                 {pages.map((page, index) => {
                     return (
                         <NavLink key={index} className={
