43a3089abd813a72dcc866dd40e37d09dbef58a5
Kashmir Shrestha <cthakashmir999@gmail.com>
2025-02-11 08:23:25 +0545

    Tensor v2.0 --small changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,8 +4,8 @@ import ContactUsMap from './contactUsMap';
 
 const Contact = () => {
   return (
-    <div className="flex flex-col items-center p-5 gap-5">
-      <h1 className="text-4xl md:text-6xl text-center text-blue-600">CONTACT US</h1>
+    <div className="flex flex-col items-center">
+      <h1 className='text-4xl font-extrabold text-center mt-10 mb-10 relative'>CONTACT US</h1>
       <div className="flex flex-col gap-10 w-full max-w-3xl">
         <ContactUsForm />
         <ContactUsMap />
